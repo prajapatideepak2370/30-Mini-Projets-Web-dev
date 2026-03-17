a0f9805fc8e03e57ca6e8746a7cc62aa655d0c8f
prajapatideepak2370 <prajapatideepak2370@gmail.com>
2026-03-17 23:01:01 +0530

    QR Generate Code

--- QR Generator/app.js
+++ QR Generator/app.js
@@ -5,5 +5,14 @@ let qrImg = document.getElementById("qrImg");
 let qrTxt = document.getElementById("qrTxt");
 
 function generateOR(){
-    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrTxt.value;
+    if (qrTxt.value.length > 0){
+        qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrTxt.value;
+        imgBox.classList.add("show-img");
+    }else{
+        qrTxt.classList.add('error');
+        setTimeout(()=>{
+            qrTxt.classList.remove("error");
+        }, 800);
+    }
+    
 }
