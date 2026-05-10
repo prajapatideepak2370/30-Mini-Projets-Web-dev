17816251232165f578801b0d4a41b17481602e75
prajapatideepak2370 <prajapatideepak2370@gmail.com>
2026-05-10 15:51:31 +0530

    Email Subs.

---
+++ Working Email/app.js
@@ -0,0 +1,16 @@
+const scriptURL = "<SCRIPT URL>";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Thank you for Subscribing!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
